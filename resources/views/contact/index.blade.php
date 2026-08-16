7b61257d361ea0d9df06e32ea44b9b4c375276f0
RakaIndraRahmawan <indrarahmawanr@gmail.com>
2026-08-16 09:44:47 +0700

    Added privacy and security related attributes to contact links

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -126,7 +126,7 @@
                             </div>
                             <div>
                                 <p class="text-[11px] text-blue-200 uppercase font-semibold">Facebook</p>
-                                <a href="https://{{ ltrim($setting->contact_facebook ?? 'facebook.com', 'https://') }}" target="_blank" class="font-bold text-white hover:text-blue-200 transition">
+                                <a href="https://{{ ltrim($setting->contact_facebook ?? 'facebook.com', 'https://') }}" target="_blank" class="font-bold text-white hover:text-blue-200 transition" ref="noopener noreferrer">
                                     {{ $setting->contact_facebook ?? 'facebook.com' }}
                                 </a>
                             </div>
@@ -138,7 +138,7 @@
                             </div>
                             <div>
                                 <p class="text-[11px] text-blue-200 uppercase font-semibold">Instagram</p>
-                                <a href="https://{{ ltrim($setting->contact_instagram ?? 'instagram.com', 'https://') }}" target="_blank" class="font-bold text-white hover:text-blue-200 transition">
+                                <a href="https://{{ ltrim($setting->contact_instagram ?? 'instagram.com', 'https://') }}" target="_blank" class="font-bold text-white hover:text-blue-200 transition" ref="noopener noreferrer">
                                     {{ $setting->contact_instagram ?? 'instagram.com' }}
                                 </a>
                             </div>
@@ -150,7 +150,7 @@
                             </div>
                             <div>
                                 <p class="text-[11px] text-blue-200 uppercase font-semibold">Twitter</p>
-                                <a href="https://{{ ltrim($setting->contact_twitter ?? 'twitter.com', 'https://') }}" target="_blank" class="font-bold text-white hover:text-blue-200 transition">
+                                <a href="https://{{ ltrim($setting->contact_twitter ?? 'twitter.com', 'https://') }}" target="_blank" class="font-bold text-white hover:text-blue-200 transition" ref="noopener noreferrer">
                                     {{ $setting->contact_twitter ?? 'twitter.com' }}
                                 </a>
                             </div>
